0a4ed7f810f07a645727b2d8ffa44e594a7332c4
alangutierrezramirez <alangutier24@gmail.com>
2026-02-24 19:37:40 +0100

    Initial Commit

---
+++ Anchor Down/Anchor Down/Anchor_DownApp.swift
@@ -0,0 +1,17 @@
+//
+//  Anchor_DownApp.swift
+//  Anchor Down
+//
+//  Created by Alan Gutierrez Ramirez on 24/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Anchor_DownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
